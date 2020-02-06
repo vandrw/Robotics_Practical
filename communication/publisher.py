4eccfa9125946bf18a91ddf00c0412dfa8e887e2
Andrei Voinea <v.andrei1455@gmail.com>
2020-02-06 14:08:57 +0100

    made publisher, not working on my pc

---
+++ communication/publisher.py
@@ -0,0 +1,31 @@
+import rospy
+from my_msgs.msg import Velocities
+
+def create_msg(wheels):
+    msg = Velocities()
+    if wheels == "both":
+        msg.left_vel = 10        # PLACEHOLDERS!
+        msg.right_vel = 10
+    elif wheels == "left":
+        msg.left_vel = 10
+        msg.right_vel = 0
+    elif wheels == "right":
+        msg.left_vel = 0
+        msg.right_vel = 10
+    else:
+        msg.left_vel = 0
+        msg.right_vel = 0
+
+    return msg
+
+def publisher_broadcast():
+    pub_left = rosply.Publisher('cool_board', Velocities, queue_size=1)
+    rate = rospy.Rate(20)
+    
+    while not rospy.is_shutdown():
+        pub.publish(create_msg("both"))
+        rate.sleep()
+        
+if __name__ == "__main__":
+    rospy.init_node("publisher_broadcast")
+    publisher_broadcast()
