c8fd97152acbb0b4eaee7e8d2d99764463a9b0be
Andrei Voinea <v.andrei1455@gmail.com>
2020-02-07 18:16:36 +0100

    made controls smoother, needs some testing

--- communication/control.py
+++ communication/control.py
@@ -48,7 +48,7 @@ def create_msg(left_wheel, right_wheel):
     msg.left_wheel = left_wheel
     msg.right_wheel = right_wheel
 
-    print("Sent: {} left, {} right".format(msg.left_wheel, msg.right_wheel))
+    print("Sent: {:.2f} left, {:.2f} right".format(msg.left_wheel, msg.right_wheel))
     return msg
 
 
@@ -59,8 +59,9 @@ def broadcast():
     while rospy.is_shutdown():
         continue
     
-    right_val = 0
-    left_val = 0
+    right_val = 0.0
+    left_val = 0.0
+    max_speed = 1.5
     
     while not rospy.is_shutdown():
         keyp = getch()
@@ -70,11 +71,14 @@ def broadcast():
         if keyp == 'w':
             right_val += 0.2
             left_val += 0.2
+            
         elif keyp == 'a':
-            right_val += 0.15
-            left_val -= 0.15
-            if (left_val < 0):
-                left_val = 0
+            right_val += 0.1
+            left_val -= 0.1
+            
+        #    if ((left_val > 0) and (right_val < 0)):
+        #         right_val = 0
+            
         elif keyp == 's':
             if ((left_val < 0) and (right_val > 0)):
                 left_val = 0
@@ -84,31 +88,50 @@ def broadcast():
 
             right_val -= 0.2
             left_val -= 0.2
+            
         elif keyp == 'd':
-            right_val -= 0.15
-            left_val += 0.15
-            if (right_val < 0):
-                right_val = 0
+            right_val -= 0.1
+            left_val += 0.1
+            
+            # if ((left_val < 0) and (right_val > 0)):
+            #     left_val = 0
+            
         elif keyp == ' ':
             right_val = 0
             left_val = 0
+            
         elif keyp == 'p':
+            right_val = 0
+            left_val = 0
             break
+        
         else:
             pass
         
-        if (left_val > 1):
-            left_val = 1
-        if (levt_val < -1):
-            left_val = -1
+        if (left_val > max_speed):
+            left_val = max_speed
+        if (left_val < -max_speed):
+            left_val = -max_speed
         
-        if (right_val > 1):
-            right_val = 1
-        if (levt_val < -1):
-            right_val = -1
+        if (right_val > max_speed):
+            right_val = max_speed
+        if (right_val < -max_speed):
+            right_val = -max_speed
 
         pub.publish(create_msg(left_val, right_val))
     
 if __name__ == "__main__":
     rospy.init_node("broadcast")
+    
+    print("Robot Controller 5000")
+    print("------------------------------------------------")
+    print("\nTo control the robot, you can press the following buttons on your keyboard:")
+    print("\tW\t-\tgo forward")
+    print("\tA\t-\tgo left")
+    print("\tD\t-\tgo right")
+    print("\tS\t-\tgo backwards")
+    print("\tSpace\t-\tbreak to a full stop")
+    print("\tP\t-\tstop the program")
+    print("\nHave fun! Press any key to begin.")
+    
     broadcast()
